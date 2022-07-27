6786477d583c25dcad1a611fd180d1ce7cc70915
MahimaGoyen <mahimagoyen@gmail.com>
2022-07-27 19:05:41 +0000

    Updated

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -32,17 +32,18 @@ async def test_seq_bug1(dut):
     clk=dut.clk.value
 
     seq_reg = "0000"
-    data=[0,1,1,0,1,1,0,1]
-    for i in range(8):
+    
+    data=[0,1,0,1,1,0,1,1]
+    for i in range(7):
+        p=0
         await FallingEdge(dut.clk)
         inp_bit = data[i]
         seq_reg=seq_reg[1:]
         seq_reg=seq_reg+str(inp_bit)
         print(seq_reg)
         if seq_reg=="1011":
-            assert dut.seq_seen.value == 0, f"test failed with seq_seen = {dut.seq_seen.value} "
-        elif seq_reg != "1011":
             assert dut.seq_seen.value == 1, f"test failed with seq_seen = {dut.seq_seen.value} "
+         
 
 
 
